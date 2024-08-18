aca79752a8cb0763add27ad0f9b094a118736d0b
Muhaimin Khan <khnmuhaimin@gmail.com>
2024-08-18 21:18:50 +0200

    Filled in equation for tim2ticks

--- Prac2/Prac2/Core/Src/main.c
+++ Prac2/Prac2/Core/Src/main.c
@@ -57,7 +57,7 @@ uint32_t triangle_LUT[NS] = { 0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176
 
 // TODO: Equation to calculate TIM2_Ticks
 
-uint32_t TIM2_Ticks = 0; // How often to write new LUT value
+uint32_t TIM2_Ticks = TIM2CLK / (F_SIGNAL * NS); // How often to write new LUT value
 uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
 /* USER CODE END PV */
 
